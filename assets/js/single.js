d82fcdebdfd716d45ba856f9746dfd1756cdec41
thisisjona <thisisjona@gmail.com>
2022-02-21 21:25:51 -0600

    added languages buttons

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,23 +1,21 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoName = function(){
-  // grab repo from url query string
+var getRepoName = function() {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  if(repoName){
-    //display repo name on the page
+
+  if (repoName) {
+    // display repo name on the page
     repoNameEl.textContent = repoName;
 
     getRepoIssues(repoName);
   } else {
-    //if no repo was given, redirect to homepage
+    // if no repo was given, redirect to the homepage
     document.location.replace("./index.html");
-  };
-  getRepoIssues(repoName);
-  repoNameEl.textContent = repoName;
+  }
 };
 
 var getRepoIssues = function(repo) {
@@ -36,10 +34,9 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      //if not succesful, redirect to homepage
-      document.location.replace("./index.hmtl");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -57,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -72,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
